47ee098ac04e0ec91ac46d41df134846158d19ca
Johan Castillo <jcjohan2707@gmail.com>
2023-08-23 15:03:07 -0400

    Estructura base para el modulo de articulos

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('articles')
-export class ArticlesController {}
+export class ArticlesController {
+
+    // Get All articles
+    @Get('')
+    async getAllArticles(){
+        
+        return {
+            status: true,
+            message: 'Articles'
+        }
+    }
+
+
+    // Create Article
+    @Post('')
+    async createArticle(){
+        
+        return {
+            status: true,
+            message: 'Create Article'
+        }
+    }
+
+    // Edit Article
+    @Put('')
+    async editArticle(){
+        
+        return {
+            status: true,
+            message: 'Edit Article'
+        }
+    }
+
+
+    // Delete Article
+    @Delete('')
+    async deleteArticle(){
+        
+        return {
+            status: true,
+            message: 'Delete Article'
+        }
+    }
+
+
+}
