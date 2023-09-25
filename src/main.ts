381bee8153ded1943c6345eae63d82738b017f06
Johan Castillo <jcjohan2707@gmail.com>
2023-09-25 16:54:49 -0400

    Se inicio la documentacion de la API

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,6 +10,27 @@ async function bootstrap() {
   const PORT = 8081
   await app.listen(PORT);
 
+
+  /*
+  #####################
+  # Documentation API #
+  #####################
+  */
+ const config = new DocumentBuilder()
+  .setTitle("Padel App API")
+  .setDescription("This is one api documentation.")
+  .setVersion("1.0")
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document);
+
+
+  /*
+  ########
+  # CORS #
+  ########
+  */
   app.enableCors({
     "origin": "*",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
